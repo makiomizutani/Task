ca9dd8639761f9101cd1db48732bdf5383f28aad
makio_mizutani <makio.mizutani0710@gamil.com>
2021-01-08 04:12:47 +0000

    rails04まで

--- rails04.rb
+++ rails04.rb
@@ -1,12 +1,12 @@
 1
-Validates :password, length: {in: 8..32}
+validates :password, length: {in: 8..32}
 
 2
 VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-Validates :password, format: { with: VALID_PASSWORD_REGEX}
+validates :password, format: { with: VALID_PASSWORD_REGEX}
 
 3
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-Validates :email, format: {with: VALID_EMAIL_REGEX}
+validates :email, format: {with: VALID_EMAIL_REGEX}
 4
-Validates :name, length: {maximum: 15}
+validates :name, length: {maximum: 15}
