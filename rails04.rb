058b5ccb9f7675bbd0ba6d0ec3c40be1b2137f73
makio_mizutani <makio.mizutani0710@gamil.com>
2021-01-13 05:46:28 +0000

    mentor task

--- rails04.rb
+++ rails04.rb
@@ -1,12 +1,8 @@
-1
-validates :password, length: {in: 8..32}
+ validates :name, presence: true, length: { maximum: 15}
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}
+  validates :password, presence: true, length: { minimum: 8, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
 
-2
-VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-validates :password, format: { with: VALID_PASSWORD_REGEX}
 
-3
-VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-validates :email, format: {with: VALID_EMAIL_REGEX}
-4
-validates :name, length: {maximum: 15}
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
